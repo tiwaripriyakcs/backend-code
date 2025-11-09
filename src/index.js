f185defec0fdd036b61729f9103f5b809c4c0e68
Priya Tiwari <your_email@example.com>
2025-11-09 06:26:36 +0530

    utils code push

--- src/index.js
+++ src/index.js
@@ -6,7 +6,18 @@ dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+connectDB()
+.then(()=>{
+  app.listen(process.env.PORT|| 8000 , ()=>{
+    console.log(`Server is running at port: ${process.env.PORT}`);
+    
+  })
+})
+.catch((err)=>{
+    console.error("❌ MongoDB connection Failed !!!:", err);
+
+})
+
 
 
 // import express from "express";
