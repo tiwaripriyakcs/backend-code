f565de2ff25f4d37e7959a4368d1c9632b4e111d
Priya Tiwari <your_email@example.com>
2025-11-14 18:43:48 +0530

    controller set down

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -5,7 +5,6 @@ import fs from "fs";
 cloudinary.config({ 
   cloud_name:process.env.CLOUDINARY_CLOUD_NAME, 
   api_key:process.env.CLOUDINARY_API, 
-  api_secret: processenv.CLOUDINARY_API_SECRET
 });
 
 const uploadOnCloudinary =async (localFilePath)=>{
